2c96b5200b851dc4a63a2aef556e51ce3941d79b
Pedro Sampaio <pedroigorjs@gmail.com>
2020-06-07 17:41:14 -0300

    Create Users migration

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,16 +4,16 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  plugins: [
-    'prettier'
-  ],
-  extends: [
-    'airbnb-base',
-  ],
+  plugins: ['prettier'],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 11,
   },
   rules: {
     'prettier/prettier': 'warn',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
