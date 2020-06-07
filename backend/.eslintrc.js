7790444d7fee7ebb3a322735a979077feb4548e8
Pedro Sampaio <pedroigorjs@gmail.com>
2020-06-07 16:56:51 -0300

    Create Initial Structure

---
+++ backend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  plugins: [
+    'prettier'
+  ],
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
---
+++ backend/src/server.js
@@ -0,0 +1,6 @@
+const { server } = require('./app');
+
+server.listen(3333, () => {
+  console.log('Server is running at http://localhost:3333');
+  console.log('To stop it press CTRL + C');
+});
