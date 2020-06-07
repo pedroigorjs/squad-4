5f27899578d73c2874105bbc9735cb1a330bb89f
Pedro Sampaio <pedroigorjs@gmail.com>
2020-06-07 18:33:57 -0300

    Create Users routes and controller

---
+++ backend/src/app/controllers/UserController.js
@@ -0,0 +1,37 @@
+const User = require('../models/User');
+
+class UserController {
+  async index(req, res) {
+    const users = await User.findAll();
+
+    return res.json(users);
+  }
+
+  async store(req, res) {
+    const { name, email, password_hash, provider } = req.body;
+
+    const userExists = await User.findOne({ where: { email } });
+
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ error: 'Users alredy exists or email not permitted.' });
+    }
+
+    const { id } = await User.create({
+      name,
+      email,
+      password_hash,
+      provider,
+    });
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+module.exports = new UserController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const routes = express.Router();
 
+const UserController = require('./app/controllers/UserController');
+
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
+
 module.exports = routes;
