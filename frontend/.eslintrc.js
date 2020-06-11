141779f95f7cf5168fe854f918d24ea44d14fe16
Pedro Sampaio <pedroigorjs@gmail.com>
2020-06-11 19:05:11 -0300

    Setting .prettierrc

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -18,10 +15,15 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
   },
 };
