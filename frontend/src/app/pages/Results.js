f6cfb640082b5e56398a6f476874a37de1d0838f
Pedro Sampaio <pedroigorjs@gmail.com>
2020-06-18 14:32:02 -0300

    Add ProfileCard to Results page

--- frontend/src/app/pages/Results.js
+++ frontend/src/app/pages/Results.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import NavBar from '../components/NavBar';
 import Filters from '../components/Filters';
+import ProfileCard from '../components/ProfileCard';
 
 function Results() {
   return (
     <>
       <NavBar bgColor="#fff" />
       <Filters />
+      <ProfileCard />
     </>
   );
 }
