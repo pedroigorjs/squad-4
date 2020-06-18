1fe58d8e9c61cbad66f7bae1955fe8209c84b077
Pedro Sampaio <pedroigorjs@gmail.com>
2020-06-18 01:25:30 -0300

    Create history for router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
+import { Router } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Home from './app/pages/Home';
 
-import Theme from './app/utils/Theme';
+import Routes from './routes';
+
+import theme from './app/utils/theme';
+import history from './app/utils/history';
 
 function App() {
   return (
-    <ThemeProvider theme={Theme}>
-      <CssBaseline />
-      <Home />
+    <ThemeProvider theme={theme}>
+      <Router history={history}>
+        <CssBaseline />
+        <Routes />
+      </Router>
     </ThemeProvider>
   );
 }
